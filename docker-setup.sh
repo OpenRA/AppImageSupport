2ea79a99ca3ce9c94a25402dee4e286ea7a09632
Paul Chote <pchote@users.noreply.github.com>
2018-07-10 19:15:38 +0000

    Add libXcursor headers.

--- docker-setup.sh
+++ docker-setup.sh
@@ -8,7 +8,7 @@ yum -y clean all
 yum -y clean expire-cache
 yum -y install epel-release
 yum -y install gcc gcc-c++ cmake patch openal-soft
-yum -y install readline-devel mesa-libGL-devel alsa-lib-devel libGLU-devel libXrender-devel libXrandr-devel
+yum -y install readline-devel mesa-libGL-devel alsa-lib-devel libGLU-devel libXrender-devel libXrandr-devel libXcursor-devel
 
 # Run the build as the default user so that generated files aren't owned by root
 su hostuser -c "./build-libraries.sh"
