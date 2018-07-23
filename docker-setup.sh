9f619a7b86c621243120d76a4150bda1987cb5d5
Paul Chote <pchote@users.noreply.github.com>
2018-07-23 15:23:28 +0100

    Remove a stray comment and whitespace.

--- docker-setup.sh
+++ docker-setup.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-
 # Install dependencies
 apt-get update
-#apt-get -y install cmake patch
 apt-get -y install curl gcc g++ make patch libopenal1 libncurses-dev libreadline-dev libgl1-mesa-dev libglu1-mesa-dev libx11-dev libxcursor-dev libxext-dev libxi-dev libxinerama-dev libxrandr-dev libxss-dev libxt-dev
 
 # Add a default user to squash unknown user errors
