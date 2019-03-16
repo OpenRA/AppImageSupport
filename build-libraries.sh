b21c85b933c43ea44f76ba13a0475c226740aff7
Paul Chote <pchote@users.noreply.github.com>
2019-03-16 10:59:55 +0000

    Remove .config overrides
    This is now done using (DY)LD_LIBRARY_PATH
    in the AppImage setup instead.

--- build-libraries.sh
+++ build-libraries.sh
@@ -4,7 +4,7 @@ OUTPUT="$(pwd)/output"
 # OpenAL-soft is copied from the debian binary package
 # TODO: Work out the correct set of dependencies / build args to produce
 # a working library when compiled from source here!
-cp /usr/lib/x86_64-linux-gnu/libopenal.so.1 "${OUTPUT}/libopenal.so"
+cp /usr/lib/x86_64-linux-gnu/libopenal.so.1 "${OUTPUT}/libopenal.so.1"
 
 # SDL2 is compiled from source
 curl -sLO http://www.libsdl.org/release/SDL2-2.0.8.tar.gz
@@ -12,7 +12,7 @@ tar xf SDL2-2.0.8.tar.gz
 pushd SDL2-2.0.8
 ./configure --prefix "$(pwd)/output" --disable-rpath --enable-sdl-dlopen --disable-loadso  --disable-audio --enable-x11-shared --disable-video-directfb --disable-video-vulkan --disable-video-dummy --disable-power --disable-joystick --disable-haptic --disable-filesystem --disable-threads --disable-file --disable-cpuinfo --disable-input-tslib
 make && make install
-cp output/lib/libSDL2.so "${OUTPUT}"
+cp output/lib/libSDL2.so "${OUTPUT}/libSDL2-2.0.so.0"
 popd
 
 # Lua 5.1 is compiled from source
@@ -21,5 +21,5 @@ tar xf lua-5.1.5.tar.gz
 pushd lua-5.1.5
 patch -p1 < ../lua51.patch
 make linux
-cp src/liblua.so.5.1 "${OUTPUT}/liblua.so"
+cp src/liblua.so.5.1 "${OUTPUT}/liblua5.1.so.0"
 popd
